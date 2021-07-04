cd23301d9506e63bb81a58f6a1063eb1f2aab984
Sayan Mallick <snyxmk@gmail.com>
2021-07-05 00:16:53 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module microservices-go
+
+go 1.16
+
+require github.com/nicholasjackson/env v0.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"log"
+	"microservices-go/handlers"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nicholasjackson/env"
+)
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	env.Parse()
+
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	// create the handlers
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	// create a new serve mux and register the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	// create a new server
+	s := &http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+	}
+
+	// start the server
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			// it is fine to use Fatal here because it is not main gorutine
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(
+		signalChan,
+		syscall.SIGHUP,  // kill -SIGHUP XXXX
+		syscall.SIGINT,  // kill -SIGINT XXXX or Ctrl+c
+		syscall.SIGQUIT, // kill -SIGQUIT XXXX
+	)
+
+	<-signalChan
+	log.Print("\nos.Interrupt - shutting down...\n")
+
+	go func() {
+		<-signalChan
+		log.Fatal("\nos.Kill - terminating...\n")
+	}()
+
+	gracefullCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelShutdown()
+
+	if err := s.Shutdown(gracefullCtx); err != nil {
+		log.Printf("shutdown error: %v\n", err)
+		defer os.Exit(1)
+		return
+	} else {
+		log.Printf("gracefully stopped\n")
+	}
+
+	cancel()
+
+	defer os.Exit(0)
+}
